529421cebe9d21c640f43a8bbbcc81bc0286e2f1
maxisun <00354615@uca.edu.sv>
2018-04-05 23:31:26 -0600

    No funciona

--- app/src/main/java/com/example/max00/acercade/MainActivity.java
+++ app/src/main/java/com/example/max00/acercade/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView twitter;
     private TextView correo;
     private TextView telefono;
-    private ImageView maxisun;
+    //private ImageView maxisun;
+    private Uri maxisun;
     private Button share;
 
     @Override
@@ -35,12 +38,28 @@ public class MainActivity extends AppCompatActivity {
         correo = findViewById(R.id.TV_emailuser);
         telefono = findViewById(R.id.TV_phone);
         share = findViewById(R.id.B_share);
-        maxisun = findViewById(R.id.IV_maxisun);
+        maxisun = Uri.parse("android.resource://res/drawable/telefono.png");
+
+        /*ArrayList<String> imageUris = new ArrayList<String>();
+        imageUris.add(imageUri1); // Add your image URIs here
+        imageUris.add(imageUri2);
+
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+        shareIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, imageUris);
+        shareIntent.setType("image/*");
+        startActivity(Intent.createChooser(shareIntent, "Share images to.."));*/
+
+        final ArrayList<String> imageUris = new ArrayList<String>();
+        imageUris.add(nombre.getText().toString()); // Add your image URIs here
+        imageUris.add(ocupacion.getText().toString());
+
 
         share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent_TEXT(nombre.getText().toString(),ocupacion.getText().toString(),github.getText().toString(),twitter.getText().toString(),correo.getText().toString(),telefono.getText().toString());
+
+                intent_TEXT();
             }
         });
 
@@ -49,19 +68,45 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void intent_TEXT(String nombre, String ocupacion, String github, String twitter, String correo, String telefono){
+    public void intent_TEXT(){
+
+        final ArrayList<String> imageUris = new ArrayList<String>();
+        imageUris.add(nombre.getText().toString()); // Add your image URIs here
+        imageUris.add(ocupacion.getText().toString());
+
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, imageUris);
+        shareIntent.setType("text/plain");
+        startActivity(Intent.createChooser(shareIntent, "Share text to.."));
+
+        /*Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, nombre.getText().toString());
+        shareIntent.putExtra(Intent.EXTRA_TEXT, ocupacion.getText().toString());
+        //shareIntent.putExtra(Intent.EXTRA_STREAM, maxisun);
+        shareIntent.setType("image/*");
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivity(Intent.createChooser(shareIntent, "Share images..."));
+
+        /*Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+        shareIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, imageUris);
+        shareIntent.setType("image/*");
+        startActivity(Intent.createChooser(shareIntent, "Share images to.."));
+
+
         Intent sendTextIntent = new Intent();
-        sendTextIntent.setAction(Intent.ACTION_SEND);
+        sendTextIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
         sendTextIntent.putExtra(Intent.EXTRA_TEXT,nombre);
         sendTextIntent.putExtra(Intent.EXTRA_TEXT,ocupacion);
         sendTextIntent.putExtra(Intent.EXTRA_TEXT,github);
         sendTextIntent.putExtra(Intent.EXTRA_TEXT,twitter);
         sendTextIntent.putExtra(Intent.EXTRA_TEXT,correo);
         sendTextIntent.putExtra(Intent.EXTRA_TEXT,telefono);
-        sendTextIntent.setType("text/plain");
         startActivity(sendTextIntent);
-/*
-        Intent shareIntent = new Intent();
+
+        /*Intent shareIntent = new Intent();
         shareIntent.setAction(Intent.ACTION_SEND);
         shareIntent.putExtra(Intent.EXTRA_STREAM, maxisun);
         shareIntent.setType("image/jpeg");
